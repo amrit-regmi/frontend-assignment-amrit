374c6d593033f5a09480159de92b42c8d8a4c343
amrit <rit.regmi@gmail.com>
2021-02-20 00:14:05 +0200

    more test for week number check

--- src/__tests__/Components/Calender/DateCell.test.tsx
+++ src/__tests__/Components/Calender/DateCell.test.tsx
@@ -3,58 +3,58 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import DateCell from '../../../Components/Calender/DateCell'
-
-import { Store } from '../../../Types/types'
-
-import  { DataProvider}  from '../../../Context/DataProvider'
-import { useDataProvider } from '../../../Context/useDataProvider'; 
+import { MonthNumber, Store, WeeklyData } from '../../../Types/types'
+import  { DataProvider }  from '../../../Context/DataProvider'
+import { useDataProvider } from '../../../Context/useDataProvider'
 import { act } from 'react-dom/test-utils'
-import { getDaysInMonth } from '../../../Utils/dateHelper'
+import { getDayNumber, getDaysInMonth, getTreatmentWeekNumber } from '../../../Utils/dateHelper'
+import data from '../../../Context/data.json'
 
 jest.mock('../../../Context/useDataProvider')
 
+
+const newTypedData:WeeklyData = {}
+const typedData = Object.assign(data,newTypedData) //TypeCast JSON data
+
+const mockDates = [1,13,16]
+const mockMonth = 1
+const mockYear =2021
+const mockDispatch = jest.fn()
+
 beforeEach(() => {
-  const mockMonth = 1
-  const mockYear =2021
-  const mockDates = [1,3,6]
-    const days = [...new Array(getDaysInMonth(mockMonth,mockYear))]
-    const mockState:Store =  {
-      calender:{ 
-        [mockYear]: {
-          [mockMonth]: days
-        }
+  const days = [...new Array(getDaysInMonth(mockMonth,mockYear))]
+  const mockState:Store =  {
+    calender:{
+      [mockYear]: {
+        [mockMonth]: days
       }
+    },
+  }
+  mockDates.forEach(date => {
+    mockState['calender'][mockYear][mockMonth][date-1] = {
+      'weekday': 'MONDAY',
+      'title': `The Meru Health Program TEST ${date}`,
+      'completed': true
     }
-    mockDates.forEach((date) => {
-      mockState['calender'][mockYear][mockMonth][date-1] = {
-        "weekday": "MONDAY",
-        "title": `The Meru Health Program TEST ${date}`,
-        "completed": true
-      }
-    }) 
-   const useDataProviderMock = useDataProvider as jest.MockedFunction<typeof useDataProvider>
-   useDataProviderMock.mockReturnValue([mockState, () => mockState])
+  })
+  const useDataProviderMock = useDataProvider as jest.MockedFunction<typeof useDataProvider>
+  useDataProviderMock.mockReturnValue([mockState, mockDispatch ])
 
 })
 
-describe ('Test the action is rendered ',  () => {
-  test('yes, if given date has action',async () => {
-  
+describe ('Test the compnent renders properly based on value from stored calender context ',  () => {
+  test('display action title if given date has action',async () => {
+    await act( async () => {
 
-   await act( async ()=> {
+      mockDates.forEach( date => {
+        const  cellData = { date :  new Date(mockYear,mockMonth,date) }
+        const  component = render(<DataProvider><DateCell {...cellData}/></DataProvider>)
+        const cell =  component.container.querySelector('div > h3')
+        expect (cell).toHaveTextContent(`The Meru Health Program TEST ${cellData.date.getDate()}`.toUpperCase())
 
-    const cellData = { date :  new Date(2021,1,1)}
+      })
 
-    const  component = render(<DataProvider><DateCell {...cellData}/></DataProvider>)
-    const cell =  component.container.querySelector('div > h3')
-    expect (cell).toHaveTextContent('The Meru Health Program TEST 1'.toUpperCase()) 
-
-    cellData.date = new Date(2021,1,3)
-    const  component2 = render(<DataProvider><DateCell {...cellData}/></DataProvider>)
-    const cell2 =  component2.container.querySelector('div > h3')
-    expect (cell2).toHaveTextContent('The Meru Health Program TEST 3'.toUpperCase()) 
-
-   })
+    })
   })
 
 })
@@ -74,7 +74,7 @@ describe ('Test the date is rendered', () => {
   test('if given date is not null, should not render date ',() => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const date: any = null
-    const cellData = { date}
+    const cellData = { date }
     const component = render(<DataProvider><DateCell {...cellData}/></DataProvider>)
     const cell =  component.container.querySelector('h2')
     expect (cell).toBe(null)
@@ -83,11 +83,42 @@ describe ('Test the date is rendered', () => {
 
   test ('Today is highlighted', () => {
     const date =new Date()
-    const cellData = {date}
+    const cellData = { date }
     const component = render(<DataProvider><DateCell {...cellData}/></DataProvider>)
     const cell =  component.container.querySelector('td')
 
     expect(cell).toHaveClass('dateCellToday')
+  })
+})
+
+
+describe('Test treatment program only starts on first full week of Month' ,() => {
+  test('disptachs update on calender if day falls on full week and action exists', () => {
+    const mockState:Store =  {
+      calender:{},
+      data: typedData
+    }
+    const useDataProviderMock = useDataProvider as jest.MockedFunction<typeof useDataProvider>
+    useDataProviderMock.mockReturnValue([mockState, mockDispatch ])
+
+    const months = [0,1,7]//run test for Jan Feb August 2021
+    months.forEach( month => {
+      const daysInmonth = getDaysInMonth(month as MonthNumber,2021)
+      for(let i = 1; i <=daysInmonth;i++  ){
+        const date = new Date(2021,month,i)
+        jest.clearAllMocks()
+        render(<DataProvider><DateCell date= {date}/></DataProvider>)
+
+        if([0,2,4].includes(getDayNumber(date))){ //If date is on Mon Wed or Fri - Based onMock Data
+          if([1,2,3].includes(getTreatmentWeekNumber(date))){
+            expect(mockDispatch).toBeCalledTimes(1)
+          }
+          else{
+            expect(mockDispatch).toBeCalledTimes(0)
+          }
+        }
+      }
+    })
   })
 })
 
